c73e30ce9ca8082526ed707f237ba807ce021aa4
RickRietbergen <rbps3.2016@gmail.com>
2022-05-16 22:45:20 +0200

    hp manager started
    hp balk toegevoegd, alleen nog maken dat je hpbar altijd naar jou toe kijkt.

--- Assets/Scripts/Player/HpManager.cs
+++ Assets/Scripts/Player/HpManager.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HpManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Slider slider;
+
+    public int maxHealth = 100;
+    public int currentHealth;
+    //player
+    public int playerHp = 100;
+    public int damagePlayer = 20;
+    //zombies
+    private void Start()
     {
-        
+        //when start, set health to maxhealth
+        currentHealth = maxHealth;
+        SetMaxHealth(maxHealth);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //if Attack, TakeDmage(value of damage)
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        SetHealth(currentHealth);
+    }
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
     }
 }
